73068717528c4e35e7a6fcc53204fe799cfca7da
karma khalefa <karrma.khalefa@gmail.com>
2026-04-27 14:27:28 +0300

    up

--- pages/login_in.php
+++ pages/login_in.php
@@ -7,7 +7,29 @@
        <link rel="stylesheet" href="../assets/login.css">
 </head>
 <body>
-    
-    
+    <div class="todo">
+<div  class="form-todo">
+       <div  class="header">
+
+ <h1>Zync System</h1>
+      <div class="line"></div>
+
+       </div>       
+<h2> Log In</h2>
+<p>Enter your credentials to access your account</p>   
+</div>
+
+<form action=""  method="POST">
+    <p>Email</p>
+<input type="text" name="email" placeholder="Confirm email">
+<p>password</p>
+<input type="password"  name="password" placeholder="Confirm Password">
+  <button  class="btn"  type="submit">Log In </button>
+    <div class="text">
+<span>Forgot your password?</span>
+<span class="form-text"> Reset Password</span>
+  </div>
+</form>
+</div>
 </body>
 </html>
